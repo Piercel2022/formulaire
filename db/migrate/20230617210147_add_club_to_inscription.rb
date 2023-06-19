bcae755018f0bad960592f50911d73a433ce133f
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-19 11:16:29 +0200

    Add the workflow

--- db/migrate/20230617210147_add_club_to_inscription.rb
+++ db/migrate/20230617210147_add_club_to_inscription.rb
@@ -1,6 +1,5 @@
 class AddClubToInscription < ActiveRecord::Migration[7.0]
   def change
     add_reference :inscriptions, :club, null: false, foreign_key: true
-    change_column_null :inscriptions, :club_id, null: true
   end
 end
