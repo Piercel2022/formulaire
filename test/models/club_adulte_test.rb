24eb936ef07087c8127255faac6dce856ff0faeb
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-19 09:03:35 +0200

    Add the workflow

---
+++ test/models/club_adulte_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ClubAdulteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/club_teevo_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ClubTeevoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
